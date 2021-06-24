7a3e190a537824558d34d3ccc81f284cd18c0b4f
akisute514 <akisute514@gmail.com>
2021-06-25 02:49:05 +0900

    selectでnullが来たときのエラーを修正&default+not_nullでの動作を変更

--- include/active_record/adaptors/sqlite3_adaptor.hpp
+++ include/active_record/adaptors/sqlite3_adaptor.hpp
@@ -24,7 +24,7 @@ namespace active_record {
         static int callback(void* res, int col_number, char** col_texts, char** col_names){
             auto result = reinterpret_cast<T*>(res);
             for(int n = 0; n < col_number; ++n){
-                result[col_names[n]].from_string(col_texts[n]);
+                result[col_names[n]].from_string((col_texts[n] == NULL) ? "null" : col_texts[n]);
             }
             return SQLITE_OK;
         }
@@ -36,7 +36,7 @@ namespace active_record {
                 *result
             );
             for(int n = 0; n < col_number; ++n){
-                attribute_string_convertors[col_names[n]]->from_string(col_texts[n]);
+                attribute_string_convertors[col_names[n]]->from_string((col_texts[n] == NULL) ? "null" : col_texts[n]);
             }
             return SQLITE_OK;
         }
@@ -46,7 +46,7 @@ namespace active_record {
             auto result = reinterpret_cast<T*>(res);
             typename T::value_type val;
             for(int n = 0; n < col_number; ++n){
-                val[col_names[n]].from_string(col_texts[n]);
+                val[col_names[n]].from_string((col_texts[n] == NULL) ? "null" : col_texts[n]);
             }
             result->push_back(val);
             return SQLITE_OK;
@@ -61,7 +61,7 @@ namespace active_record {
                 val
             );
             for(int n = 0; n < col_number; ++n){
-                attribute_string_convertors[col_names[n]]->from_string(col_texts[n]);
+                attribute_string_convertors[col_names[n]]->from_string((col_texts[n] == NULL) ? "null" : col_texts[n]);
             }
             result->push_back(val);
             return SQLITE_OK;
@@ -164,16 +164,16 @@ namespace active_record {
         static active_record::string column_definition() {
             return active_record::string{ T::column_name }
                 + " TEXT"
-                + (T::has_constraint(T::not_null) ? " NOT NULL" : "")
                 + (T::has_constraint(T::unique) ? " UNIQUE" : "")
                 + (T::has_constraint(T::primary_key) ? " PRIMARY KEY" : "")
+                + (T::has_constraint(T::not_null) ? " NOT NULL" : "")
                 + (T::has_constraint(typename T::constraint_default_value_impl{}) ?
-                    active_record::string{" DEFAULT '"}
-                        + active_record::sanitize(T::get_constraint(typename T::constraint_default_value_impl{})->template target<typename T::constraint_default_value_impl>()->default_value) + "'"
+                    active_record::string{ (T::has_constraint(T::not_null) ? " ON CONFLICT REPLACE" : "") } + " DEFAULT '"
+                    + active_record::sanitize(T::get_constraint(typename T::constraint_default_value_impl{})->template target<typename T::constraint_default_value_impl>()->default_value) + "'"
                     : "")
                 + (T::has_constraint(typename T::constraint_length_impl{}) ?
                     active_record::string{ " CHECK(length(" } + active_record::string{ T::column_name }
-                        + ")<=" + std::to_string(T::get_constraint(typename T::constraint_length_impl{})->template target<typename T::constraint_length_impl>()->length) + ")"
+                    + ")<=" + std::to_string(T::get_constraint(typename T::constraint_length_impl{})->template target<typename T::constraint_length_impl>()->length) + ")"
                     : "")
                 + reference_definition<T>();
         }
@@ -183,12 +183,12 @@ namespace active_record {
         static active_record::string column_definition() {
             return active_record::string{ T::column_name }
                 + active_record::string{ " INTEGER" }
-                + (T::has_constraint(T::not_null) ? " NOT NULL" : "")
                 + (T::has_constraint(T::unique) ? " UNIQUE" : "")
                 + (T::has_constraint(T::primary_key) ? " PRIMARY KEY" : "")
+                + (T::has_constraint(T::not_null) ? " NOT NULL" : "")
                 + (T::has_constraint(typename T::constraint_default_value_impl{}) ?
-                    active_record::string{" DEFAULT "}
-                        + std::to_string(T::get_constraint(typename T::constraint_default_value_impl{})->template target<typename T::constraint_default_value_impl>()->default_value)
+                    active_record::string{ (T::has_constraint(T::not_null) ? " ON CONFLICT REPLACE" : "") } + " DEFAULT "
+                    + std::to_string(T::get_constraint(typename T::constraint_default_value_impl{})->template target<typename T::constraint_default_value_impl>()->default_value)
                     : "")
                 + reference_definition<T>();
         }
@@ -198,12 +198,12 @@ namespace active_record {
         static active_record::string column_definition() {
             return active_record::string{ T::column_name }
                 + active_record::string{ " REAL" }
-                + (T::has_constraint(T::not_null) ? " NOT NULL" : "")
                 + (T::has_constraint(T::unique) ? " UNIQUE" : "")
                 + (T::has_constraint(T::primary_key) ? " PRIMARY KEY" : "")
+                + (T::has_constraint(T::not_null) ? " NOT NULL" : "")
                 + (T::has_constraint(typename T::constraint_default_value_impl{}) ?
-                    active_record::string{" DEFAULT "}
-                        + std::to_string(T::get_constraint(typename T::constraint_default_value_impl{})->template target<typename T::constraint_default_value_impl>()->default_value)
+                    active_record::string{ (T::has_constraint(T::not_null) ? " ON CONFLICT REPLACE" : "") } + " DEFAULT "
+                    + std::to_string(T::get_constraint(typename T::constraint_default_value_impl{})->template target<typename T::constraint_default_value_impl>()->default_value)
                     : "")
                 + reference_definition<T>();
         }
--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ struct Member : public active_record::model<Member> {
     struct Name : public active_record::attributes::string<Member, Name> {
         using active_record::attributes::string<Member, Name>::string;
         static constexpr auto column_name = "name";
-        inline static const auto constraints = { not_null, length(64) };
+        inline static const auto constraints = { length(8), default_value("unknown") };
     } name;
     std::tuple<ID&, Name&> attributes = std::tie(id, name);
 };
@@ -36,28 +36,14 @@ struct EnteringLog : public active_record::model<EnteringLog> {
 int main() {
     constexpr Member::ID id = 10;
     Member::Name name = "test";
-    //id = std::nullopt;
-    constexpr auto table_name = id.column_full_name().first;
-    std::cout << id.column_full_name().first << std::endl;
-    std::cout << Member::column_names()[0] << std::endl;
-    std::cout << EnteringLog::MemberID{}.column_full_name().first << std::endl;
-    std::cout << id.to_string() << std::endl;
-    std::cout << "primary key: " << Member::ID::is_primary_key << std::endl;
-
     Member member;
     member.id = 123;
     member.name = "nkodice";
-    std::cout << member.get_attribute_strings()[1] << std::endl;
-    std::cout << member["name"].to_string() << std::endl;
 
     std::array<Member, 4> members;
-    std::cout << Member::insert(members).to_sql() << std::endl;
-    std::cout << Member::all().to_sql() << std::endl;
-    std::cout << Member::select(Member::ID{}).to_sql() << std::endl;
-    std::cout << Member::pluck(Member::ID{}).to_sql() << std::endl;
-    std::cout << Member::where(Member::ID{10}, Member::Name::like("nko\\\' OR 1=1;--dice表")).to_sql() << std::endl;
-    std::cout << Member::where(Member::ID::in(10,20,30)).to_sql() << std::endl;
-
+    members[0].name = "satou";
+    members[1].name = "sasaki";
+    members[1].name = "watanabe aaaaaaa";
     std::cout << "\033[33m[connecting] \033[m" << std::endl;
     auto connection = active_record::sqlite3_adaptor::open("test.sqlite3", active_record::sqlite3::options::create);
     if(connection.has_error()){
@@ -80,8 +66,13 @@ int main() {
     else std::cout << "\033[32m done! \033[m" << std::endl;
 
     std::cout << "\033[33m[execution] \033[m" << Member::insert(member).to_sql() << std::endl;
-    connection.exec(Member::insert(member));
-    if(connection.has_error()){
+    if(connection.exec(Member::insert(member))){
+        std::cout << "\033[31m" << connection.error_message() << "\033[m" << std::endl;
+    }
+    else std::cout << "\033[32m done! \033[m" << std::endl;
+
+    std::cout << "\033[33m[execution] \033[m" << Member::insert(members).to_sql() << std::endl;
+    if(connection.exec(Member::insert(members))){
         std::cout << "\033[31m" << connection.error_message() << "\033[m" << std::endl;
     }
     else std::cout << "\033[32m done! \033[m" << std::endl;
